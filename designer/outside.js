c9acd4f7591f438c4cecad01d53b71ae75acbae3
neciacodes8 <lilmzsweetz@gmail.com>
2024-01-01 20:29:39 -0800

    added api to change city and temperature in app

--- designer/outside.js
+++ designer/outside.js
@@ -1,12 +1,25 @@
-
-function findFormResults (event){
-    event.preventDefault ();
-    let barSearcher = document.querySelector("#search-bar");
-    let cityNameElement = document.querySelector("#city-name");
-    cityNameElement.innerHTML=barSearcher.value ;
-
+function weatherUpdate(response) {
+  let temperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#temp-number");
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
 }
 
+function cityLookUp(city) {
+  let apiKey = "59b3e3d0a54fofe83cd678bccc0837t3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(weatherUpdate);
+}
+
+function findFormResults(event) {
+  event.preventDefault();
+  let barSearcher = document.querySelector("#search-bar");
+  let cityNameElement = document.querySelector("#city-name");
+  cityNameElement.innerHTML = barSearcher.value;
+  cityLookUp(barSearcher.value);
+}
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",findFormResults);
+searchFormElement.addEventListener("submit", findFormResults);
