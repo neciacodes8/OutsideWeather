eebcfb864f84b8b249a4f198b378d367314a537c
neciacodes8 <lilmzsweetz@gmail.com>
2024-01-02 21:11:30 -0800

    Used API to update description, date/time, wind, and humidity

--- designer/outside.js
+++ designer/outside.js
@@ -4,6 +4,44 @@ function weatherUpdate(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#type");
+  descriptionElement.innerHTML= response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+
+  let windElement = document.querySelector("#wind-speed");
+windElement.innerHTML=`${response.data.wind.speed}mph`;
+
+  let clockElement = document.querySelector("#clock-day");
+  let date = new Date(response.data.time * 1000);
+  clockElement.innerHTML = formatDate(date);
+
+  
+  
+}
+
+function formatDate(date){
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let day = weekDays[date.getDay()];
+
+  if(minutes <10){minutes = `0${minutes}`;}
+
+  return `${day} ${hours}:${minutes}`;
+
 }
 
 function cityLookUp(city) {
